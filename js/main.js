db6d5ce9f33a9116261766ac8b0fbdd619705cc1
Florencia Smith Vera <florenciaa.smith@gmail.com>
2024-07-11 04:57:29 -0300

    calculo tradein ok

--- js/main.js
+++ js/main.js
@@ -15,13 +15,30 @@ const valorcien = (a, b, c) => {
     let patentes = parseInt(prompt("Ingresa valor deuda patentes"))
     let infracciones = parseInt(prompt("Ingresa valor deuda infracciones"))
     alert("El valor total del auto tomado es ARS " + (montocomprob - patentes - infracciones))
-    let valorcien = (montocomprob - patentes - infracciones) 
+    let valorcien = parseInt((montocomprob - patentes - infracciones))
     return valorcien
 }
 
+function tradein() {
+    let montocomprob = parseInt(prompt("Ingresa valor monto comprobante"))
+    let patentes = parseInt(prompt("Ingresa valor deuda patentes"))
+    let infracciones = parseInt(prompt("Ingresa valor deuda infracciones"))
+    alert("El valor total del auto tomado es ARS " + (montocomprob - patentes - infracciones) + "\n\nACORDATE de ingresarlo en la prox pantalla!!")
+    let valorcien = parseInt(prompt("Ingresa el valor total del auto tomado"))
+    let valorventa = parseInt(prompt("Ingresa el valor del auto que lleva"))
+    let saldoafavor = parseInt((valorventa - valorcien))
+    if (saldoafavor > 0) {
+        alert("El cliente aún debe ARS " + (valorventa - valorcien))
+    } else if (saldoafavor < 1) {
+        alert("Transferir al cliente ARS " + (-(valorventa - valorcien)))
+    }
+    else {
+        alert("Ingresá una opción valida por favor")
+    }
+}
 
 do {
-    calcuTeso = prompt("Seleccione la operación a realizar:\n\n1. TOMA 100%\n2. TOMA 80/20\n3. TRADE IN\n\nPara salir escriba CERRAR SESION").toUpperCase()
+    calcuTeso = prompt("Seleccione la operación a realizar:\n\n1. TOMA 100%\n2. TRADE IN\n\nPara salir escriba CERRAR SESION").toUpperCase()
     if (calcuTeso === "CERRAR SESION") {
         alert("Gracias!\nNo te olvides del asiento contable!")
     } else if (calcuTeso == "1") {
@@ -31,14 +48,10 @@ do {
         } while (montocompro === "CERRAR SESION")
     } else if (calcuTeso == "2") {
         montocompro()
-    } else if (calcuTeso == "3") {
-        montocompro()
+        do {
+            tradein()
+        } while (montocompro === "CERRAR SESION")
     } else {
         alert("Ingresá una opción valida por favor")
     }
 } while (calcuTeso !== "CERRAR SESION")
-
-
-
-
-
